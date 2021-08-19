579cf7e6dfc8d971a169ea86378cd6f51886090f
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-19 00:32:53 -0300

    implement the heart

--- files/playing.py
+++ files/playing.py
@@ -352,7 +352,7 @@ class Playing(GameElement):
                           4: self.images['high_fence'], 5: self.images['spawn_wall'],
                           6: self.images['heavy_mud'], 7: self.images['light_mud'], 
                           8: self.images['barbwire'], 9: self.images['bad_wall'], 
-                          10: self.images['bullets'], 11: cores.VERMELHO,
+                          10: self.images['bullets'], 11: self.images['heart'],
                           12: cores.MARROM_FEIO}
             
             color = all_colors[column]
